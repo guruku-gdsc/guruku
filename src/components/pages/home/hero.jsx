8cd2aa917a3457ed6b97447b53eaf91e9f323412
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-06-14 15:01:58 +0700

    test: testing emote in commmit :tada:

--- src/components/pages/home/hero.jsx
+++ src/components/pages/home/hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <div className="min-h-screen bg-green">
       <div className="wrapper">
-        <h1>Hero Section</h1>
+        <h1>Hero Section?</h1>
       </div>
     </div>
   );
