6004614c882e8ad9692173e0d036faa773937683
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-06-19 00:40:43 +0700

    feat: add detail guru card

--- src/pages/guru/index.jsx
+++ src/pages/guru/index.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/common/button";
 import { Navbar } from "@/components/common/layouts";
 import Image from "next/image";
 
@@ -35,8 +36,61 @@ export default function Guru() {
                 alt=""
                 className="rounded-2xl"
               />
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col items-center gap-2">
                 <p className="text-xl font-semibold">Aliya Manroe</p>
+                <div className="flex items-center gap-1">
+                  <div className="flex">
+                    <Image
+                      src={"/svg/icon-star.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                    <Image
+                      src={"/svg/icon-star.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                    <Image
+                      src={"/svg/icon-star.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                    <Image
+                      src={"/svg/icon-star.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                    <Image
+                      src={"/svg/icon-star.svg"}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
+                  </div>
+                  <p className="text-sm text-grey1">(140)</p>
+                </div>
+              </div>
+              <div className="w-full flex flex-col gap-4">
+                <div className="flex justify-between items-center text-sm">
+                  <p className="text-grey1">Tarif per jam</p>
+                  <p className="font-semibold">Rp 200.000</p>
+                </div>
+                <div className="flex justify-between items-center text-sm">
+                  <p className="text-grey1">Waktu respon</p>
+                  <p className="font-semibold">30 menit</p>
+                </div>
+                <div className="flex justify-between items-center text-sm">
+                  <p className="text-grey1">Jumlah murid</p>
+                  <p className="font-semibold">200+</p>
+                </div>
+              </div>
+              <div className="w-full flex flex-col items-center gap-4">
+                <Button>Reservasi Kursus</Button>
+                <p className="text-sm text-green1">1x percobaan gratis</p>
               </div>
             </div>
           </div>
