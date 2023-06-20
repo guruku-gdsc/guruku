29dba7f6c64d0271829d17f4660d31f3484696ab
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-06-20 21:32:30 +0700

    feat: add error blank text

--- src/pages/cari-guru/index.jsx
+++ src/pages/cari-guru/index.jsx
@@ -128,11 +128,15 @@ export default function CariGuru() {
           </div>
         </div>
         <div className="grid grid-cols-2 mt-10 gap-y-10 gap-x-8">
-          {filteredTopTeacherList.map((item) => (
-            <Link key={item.id} href={`/guru/${item.id}`}>
-              <TeacherCard item={item} />
-            </Link>
-          ))}
+          {filteredTopTeacherList.length > 0 ? (
+            filteredTopTeacherList.map((item) => (
+              <Link key={item.id} href={`/guru/${item.id}`}>
+                <TeacherCard item={item} />
+              </Link>
+            ))
+          ) : (
+            <p>Tidak ada guru yang ditemukan</p>
+          )}
         </div>
       </div>
     </div>
